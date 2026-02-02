558f0c3c3390a55970634e8ee71838d1acc1e9a9
SChaparro <81198964+SantiChaparro@users.noreply.github.com>
2026-02-02 20:29:21 -0300

    arreglos de variables de entorno

--- client/src/assets/urlApi.js
+++ client/src/assets/urlApi.js
@@ -1,8 +1,6 @@
-// export const urlApi='https://server-bendahan.onrender.com/'
-//export const urlApi='http://localhost:3001/'
-const urlApiL =
+
+export const urlApi =
   process.env.NODE_ENV === "production"
     ? process.env.REACT_APP_API_URL
-    : "http://localhost:3001";
+    : "http://localhost:3001/";
 
-export default urlApiL;
