0f993155a2228979563ac8c48f4963f69eb92c7e
SChaparro <81198964+SantiChaparro@users.noreply.github.com>
2026-02-03 15:47:47 -0300

    arreglando url para produccion

--- server/src/routes/ratingRouter.js
+++ server/src/routes/ratingRouter.js
@@ -8,7 +8,7 @@ ratingRouter.get('/urlRating',getUrlRating);
 // Redirigir desde WhatsApp a la vista del frontend
 ratingRouter.get('/confirm-rating', (req, res) => {
     const { appointmentId, tenantId, professionalDni, score } = req.query;
-    const frontendUrl = process.env.REACT_APP_FRONT_URL || process.env.TUNNEL_URL || 'http://localhost:3000';
+    const frontendUrl = process.env.FRONTEND_URL;
     res.redirect(`${frontendUrl}/confirm-rating?appointmentId=${appointmentId}&tenantId=${tenantId}&professionalDni=${professionalDni}&score=${score}`);
 });
 ratingRouter.get("/submitRating", submitRatingHandler);
